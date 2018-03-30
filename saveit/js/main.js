04efb979d69c44129c7b2b8ce5ef5558f53815ac
LCgenial <35601827+Louis-ClementLangue@users.noreply.github.com>
2018-03-30 14:34:20 +0200

    Update Main.js
    Merci de vérifier qu'au prochaines modifications les games.state.add introduction, continuer et credit.

--- saveit/js/main.js
+++ saveit/js/main.js
@@ -1,5 +1,8 @@
 var game  = new Phaser.Game(1200, 600, Phaser.CANVAS,'');
+game.state.add('Introduction', Introduction);
 game.state.add('Game', Game);
+game.state.add('Continuer', Continuer);
+game.state.add('Credit', Credit);
 game.state.add('Menu', Menu);
-game.state.start('Game');
-//game.state.start('Menu');
+//game.state.start('Game');
+game.state.start('Menu');
