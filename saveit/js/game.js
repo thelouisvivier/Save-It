8ac140fcb67e15ba1cced83044d1a57af011bc97
garmatueur <remi.vandewalle@isen.yncrea.fr>
2018-04-06 14:36:03 +0200

    new sprite + new interactions

--- saveit/js/game.js
+++ saveit/js/game.js
@@ -1,12 +1,18 @@
 var louis;
 var tvgroup;
 var tv;
+var lampeB;
+var evier;
 var cursors;
 var timer;
 var total = 3;
-var keyA;
-var keyAdown=false;
+var keyT;
+var keyTdown=false;
+var keyL;
+var keyLdown=false;
 var tvstate=true;
+var lampeBstate=true;
+var evierstate=true;
 var flipFlop;
 var Game = {
   preload: function (){
@@ -14,6 +20,9 @@ var Game = {
     game.load.image('Game Over', 'assets/GameOver.png');
     game.load.spritesheet('louis', 'assets/louis.png', 100, 95);
     game.load.spritesheet('tv', 'assets/tv.png', 100, 100);
+    game.load.spritesheet('lampeB', 'assets/lampeB.png', 100, 256);
+    game.load.spritesheet('lampeB', 'assets/lampeB.png', 100, 256);
+    game.load.spritesheet('evier', 'assets/evier.png', 200, 158);
   },
 
 
@@ -23,19 +32,31 @@ var Game = {
     game.add.tileSprite(0, 0, 1920, 1920, 'sol');
 
     game.world.setBounds(0, 0, 1920, 1920);
-
+    tv=game.add.sprite(1200,800,'tv')
+    evier=game.add.sprite(1400,800,'evier')
+    game.physics.enable(tv, Phaser.Physics.ARCADE);
+    game.physics.enable(evier, Phaser.Physics.ARCADE);
+    tv.body.immovable = true;
+    evier.body.immovable = true;
     louis = game.add.sprite(game.world.centerX, game.world.centerY, 'louis');
     //louis.scale.setTo(0.5, 0.5);
+    //louis.body.collideWorldBounds = true;
+game.physics.enable(louis, Phaser.Physics.ARCADE);
 
-    tv=game.add.sprite(1000,800,'tv')
+    tv.body.setSize(70, 20, 10); //droite,bas,décalage
+
+    lampeB=game.add.sprite(1000,800,'lampeB')
+    lampeB.scale.setTo(0.5, 0.5);
+    game.physics.enable(lampeB, Phaser.Physics.ARCADE);
+    lampeB.body.immovable = true;
+    lampeB.body.setSize(60, 40,10,220);//lampe par derrière
+    //changer les parametres si on veut passer devant la lampe.
 
 
-    game.physics.enable(louis, Phaser.Physics.ARCADE);
-    game.physics.enable(tv, Phaser.Physics.ARCADE);
-    tv.body.immovable = true;
-    louis.body.collideWorldBounds = true;
 
+    evier.scale.setTo(0.5, 0.5);
 
+    evier.body.setSize(180,0);
 
     cursors = game.input.keyboard.createCursorKeys();
     game.camera.follow(louis, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
@@ -46,46 +67,91 @@ var Game = {
     //  Start the timer running - this is important!
     //  It won't start automatically, allowing you to hook it to button events and the like.
     timer.start();
-    //  We will enable physics for any star that is created in this group
-    //  Here we'll create 12 of them evenly spaced apart
-    //  Create a star inside of the 'stars' group
+
     louis.animations.add('up', [9, 10, 11], 10, true);
     louis.animations.add('down', [12, 13, 14], 10, true);
     louis.animations.add('left', [0, 1, 2, 3], 10, true);
     louis.animations.add('right', [5, 6, 7, 8], 10, true);
-    tv.animations.add('on', [0, 1, 2, 3, 4, 5, 6], 5, true);
-    tv.animations.add('off', [7], 5, true);
+    tv.animations.add('tvon', [0, 1, 2, 3, 4, 5, 6], 5, true);
+    tv.animations.add('tvoff', [7], 5, true);
+    lampeB.animations.add('lampeBon', [0], 5, true);
+    lampeB.animations.add('lampeBoff', [1], 5, true);
+    evier.animations.add('evieron', [0,1,2], 5, true);
+    evier.animations.add('evieroff', [3], 5, true);
 
-    keyA = game.input.keyboard.addKey(Phaser.Keyboard.A);
+    keyT = game.input.keyboard.addKey(Phaser.Keyboard.T);
+    keyL = game.input.keyboard.addKey(Phaser.Keyboard.L);
   },
 
   update : function ()
   {
     this.game.physics.arcade.collide(louis, tv);
-
+    this.game.physics.arcade.collide(louis, lampeB);
+    this.game.physics.arcade.collide(louis, evier);
     //Test de la distance entre le joueur et la télé, à reformuler
     //https://stackoverflow.com/questions/29922024/how-to-set-keyboard-key-pressed-event-only-once-after-pressed-but-not-continousl
-    if(louis.body.x-tv.body.x<=100){
-      if(tv.body.x-louis.body.x<=100){
-        if(louis.body.y-tv.body.y<=100){
-          if(tv.body.y-louis.body.y<=100){
-            if (keyA.isDown){
-              if (!flipFlop) {
-                tvstate^=true
-                flipFlop = true;
-              }
-            }
-            if (keyA.isUp) {
-              flipFlop = false;
-            }
-          }
-        }}}
+    if(louis.body.x-tv.body.x<=100 && tv.body.x-louis.body.x<=100 && louis.body.y-tv.body.y<=100 && tv.body.y-louis.body.y<=100){
+      if (keyT.isDown){
+        if (!flipFlop) {
+          tvstate^=true
+          flipFlop = true;
+        }
+      }
+      if (keyT.isUp) {
+        flipFlop = false;
+      }
+    }
+
+
         if (tvstate==true){
-          tv.animations.play('on');
+          tv.animations.play('tvon');
         }
         else if (tvstate==false) {
-          tv.animations.play('off')
+          tv.animations.play('tvoff')
         }
+
+
+        if(louis.body.x-lampeB.body.x<=100 && lampeB.body.x-louis.body.x<=100 && louis.body.y-lampeB.body.y<=100 && lampeB.body.y-louis.body.y<=100){
+          if (keyL.isDown){
+            if (!flipFlop) {
+              lampeBstate^=true
+              flipFlop = true;
+            }
+          }
+          if (keyL.isUp) {
+            flipFlop = false;
+          }
+        }
+
+
+            if (lampeBstate==true){
+              lampeB.animations.play('lampeBon');
+            }
+            else if (lampeBstate==false) {
+              lampeB.animations.play('lampeBoff')
+            }
+
+
+            if(louis.body.x-evier.body.x<=100 && evier.body.x-louis.body.x<=100 && louis.body.y-evier.body.y<=100 && evier.body.y-louis.body.y<=100){
+              if (keyL.isDown){
+                if (!flipFlop) {
+                  evierstate^=true
+                  flipFlop = true;
+                }
+              }
+              if (keyL.isUp) {
+                flipFlop = false;
+              }
+            }
+
+
+                if (evierstate==true){
+                  evier.animations.play('evieron');
+                }
+                else if (evierstate==false) {
+                  evier.animations.play('evieroff')
+                }
+
         if (cursors.up.isDown){
           //louis.body.moveUp(300)
           louis.body.velocity.y = -300;
@@ -120,7 +186,6 @@ var Game = {
         },
 
         updateCounter : function (){
-          game.add.tileSprite(400, 300, 200, 200, 'Game Over');
           if(tvstate==true){
             total--;
           };
