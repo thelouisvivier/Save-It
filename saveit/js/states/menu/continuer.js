c73cc800777448a3ddf4df1676894d1c30d61805
LCgenial <35601827+Louis-ClementLangue@users.noreply.github.com>
2018-04-11 19:21:05 +0200

    Finish selection level
    le menu "continuer" est complet on peut désormais choisir le niveau que l'on veut faire

--- saveit/js/states/menu/continuer.js
+++ saveit/js/states/menu/continuer.js
@@ -2,6 +2,8 @@ var Continuer = {
   preload : function(){
     game.load.image('menu','./assets/textures/menu.jpg',4000,3000);
     game.load.spritesheet('retour','./assets/buttons/retour.png', 0, 74);
+    game.load.spritesheet('niveau1','./assets/buttons/niveau1.png', 0, 87);
+    game.load.spritesheet('niveau2','./assets/buttons/niveau2.png', 0, 87);
   },
 
   create : function(){
@@ -11,9 +13,19 @@ var Continuer = {
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     game.scale.setShowAll();
     var boutonRetour = game.add.button(1000, 500, 'retour', this.retourMenu, this, 1, 0);
+    var boutonNiveau1 = game.add.button(400, game.world.centerY, 'niveau1', this.accesNiveau1, this, 1, 0);
+    var boutonNiveau2 = game.add.button(700, game.world.centerY, 'niveau2', this.accesNiveau2, this, 1, 0);
   },
 
   retourMenu : function(){
     game.state.start('MenuState');
+  },
+
+  accesNiveau1 : function(){
+    game.state.start('GameState');
+  },
+
+  accesNiveau2 : function(){
+    game.state.start('GameState2');
   }
 }
