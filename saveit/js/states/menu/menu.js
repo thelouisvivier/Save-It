ac83c3b1d9d8170e51c050432374ee5b414432c5
LCgenial <35601827+Louis-ClementLangue@users.noreply.github.com>
2018-04-13 14:11:51 +0200

    Move Button
    Léger deplacement à gauche dans le menu

--- saveit/js/states/menu/menu.js
+++ saveit/js/states/menu/menu.js
@@ -14,10 +14,10 @@ var Menu = {
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     game.scale.setShowAll();
 
-    var buttonIntroduction = game.add.button(565, 200, 'b_intro', this.startIntroduction, this, 1, 0, 2).scale.setTo(0.25,0.25);
-    var buttonDemarrer = game.add.button(565, 300, 'b_demarrer', this.startStart, this, 1, 0, 2).scale.setTo(0.25,0.25);
-    var buttonContinuer = game.add.button(565, 400, 'b_continuer', this.startContinuer, this, 1, 0, 2).scale.setTo(0.25,0.25);
-    var buttonCredits = game.add.button(565, 500, 'b_credits', this.startCredit, this, 1, 0, 2).scale.setTo(0.25,0.25);
+    var buttonIntroduction = game.add.button(600, 200, 'b_intro', this.startIntroduction, this, 1, 0, 2).scale.setTo(0.25,0.25);
+    var buttonDemarrer = game.add.button(600, 300, 'b_demarrer', this.startStart, this, 1, 0, 2).scale.setTo(0.25,0.25);
+    var buttonContinuer = game.add.button(600, 400, 'b_continuer', this.startContinuer, this, 1, 0, 2).scale.setTo(0.25,0.25);
+    var buttonCredits = game.add.button(600, 500, 'b_credits', this.startCredit, this, 1, 0, 2).scale.setTo(0.25,0.25);
   },
 
   startIntroduction : function(){
