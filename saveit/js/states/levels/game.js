eae875ef8e7d6e5e973782ba4ef7c6f2a761fdcf
Ployt <florentin.dehooghe@isen.yncrea.fr>
2018-04-21 12:11:33 +0200

    debug allumage + decompteur
    Reste à debug le menu pause

--- saveit/js/states/levels/game.js
+++ saveit/js/states/levels/game.js
@@ -31,10 +31,10 @@ var Game = {
     Appliances.tv.create(1200,800,'noir',0/*id UNIQUE de l'objet*/);
     Appliances.lampe.create(1000,600,'jaune',1);
     Appliances.radiateur.create(1300,700,2);
-    Appliances.microonde.create(1400,800,3);
-    Appliances.radio.create(900,800,4);
-    Appliances.fenetre.create(600,600,1/*model de fenetre*/,5);
-    Appliances.ventilateur.create(1000,1000,6);
+    //Appliances.microonde.create(1400,800,3);
+    Appliances.radio.create(900,800,3);
+    Appliances.fenetre.create(600,600,1/*model de fenetre*/,4);
+    Appliances.ventilateur.create(1000,1000,5);
     //Popup.create('lampeinfo',1000,600);
 
     //Ajout du perso
@@ -55,7 +55,7 @@ var Game = {
     console.log(spriteStorage[1]);
     console.log(spriteStorage[2]);
 
-<<<<<<< HEAD
+//<<<<<<< HEAD
     for (o of stateStorage){
     nbobj++;};
 
@@ -64,11 +64,11 @@ var Game = {
 
 
 
-=======
+//=======
     //Menu Pause
-    menuPause();
+/*    menuPause();
 >>>>>>> 265442f29ff0fd016e1989e1eb09bc3156fc8e80
-
+*/
   },
 
   update : function (){
@@ -77,10 +77,10 @@ var Game = {
     Appliances.tv.update(Keys.T(),0);// Touche pour contôler l'objet et ID unique de cet objet
     Appliances.lampe.update(Keys.L(),1);
     Appliances.radiateur.update(Keys.R(),2);
-    Appliances.microonde.update(Keys.M(),3);
-    Appliances.radio.update(Keys.R(),4);
-    Appliances.fenetre.update(Keys.F(),5);
-    Appliances.ventilateur.update(Keys.V(),6);
+  //  Appliances.microonde.update(Keys.M(),3);
+    Appliances.radio.update(Keys.R(),3);
+    Appliances.fenetre.update(Keys.F(),4);
+    Appliances.ventilateur.update(Keys.V(),5);
 
     if(total==0){
       timer.stop();
@@ -97,7 +97,10 @@ var Game = {
   },
   countobjon :function ()
   {
-    for(i of spriteStorage){
+    var i=0;
+    for(var i of spriteStorage){
+      console.log(i);
+
       if(i.key=='tv2' && stateStorage[i.z-1]){
         objectcost.forEach(function(element) {
           index = objectcost.indexOf(element);
@@ -137,7 +140,6 @@ var Game = {
 },
 allumeobj :function()
 {
-  console.log(coutobjprecedent);
   if(coutobjprecedent <= 1.75)
   {
     turnon = Math.random() * (nbobj);
