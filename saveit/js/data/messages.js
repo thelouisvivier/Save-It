452468559d0c05214ea844a4d551479c84f873d9
garmatueur <remi.vandewalle@isen.yncrea.fr>
2018-05-09 14:09:28 +0200

    texte pop up

--- saveit/js/data/messages.js
+++ saveit/js/data/messages.js
@@ -1,5 +1,18 @@
 var datamessages = [
-    {name:'tvinfo', text:'Voici la tv. Cet objet du quotidien n\'est pas forcément le plus consommateur du foyer. En revanche il devrait consommer beaucoup moins si on évitait de le laisser en veille inutilement. Pour info il vous en coûtera environ 16€/an d\'électricité.'},
-    {name:'lampeinfo', text:'Voici une lampe. Malgré son utilité indiscutable, une lampe reste encore trop souvent allumée inutilement. En évitant de la laisser allumée 2h de trop par jour, vous économiserez 10€/an. Donc privilégiez la lumière naturelle !'},
-    {name:'evierinfo', text:''}
+    {name:'tvinfo', text:'Voici la télévision. Cet objet du quotidien n\'est pas forcément le plus consommateur du foyer il consommerait beaucoup moins si on évitait de le laisser en veille inutilement. Pour info, la télévision vous coûtera environ 16€/an d\'électricité alors éteignez la avec la touche 't'.'},
+    {name:'lampeinfo', text:'Voici la lampe. Malgré son utilité indiscutable, une lampe reste encore trop souvent allumée inutilement. En évitant de la laisser allumée 2h de trop par jour, vous économiserez 10€/an. Privilégiez donc la lumière naturelle en éteignant la lampe grace à la touche 'l'.'},
+    {name:'evierinfo', text:'Voici l\'évier. En moyenne, 40 litres d\'eau sont consommés pour une vaisselle à la main. Si on ajoute à cela l\'eau qui coule lorsque le robinet est mal fermé ou lorsque nous lavons nos dents, les litres s\'écoulent. Coupez donc l\'eau lorsqu\'elle coule inutilement grace à la touche 'e'.'}
+    {name:'fenetreinfo', text:'Voici la fenêtre. C\' élément ne consomme rien à proprement parlé mais si vous mettez du chauffage dans votre maison, il est préférable des la fermer. Faites le grace à la touche 'f''}
+    {name:'appliquesinfo', text:'Voici l\'applique murale. Bon concrétement qui appelle ça comme ça ? Personne ? Ok donc ce sera des lumières. Il faut bien évidemment les éteindre lorsque nous ne sommes pas dans une pièce pour éviter de consommé inutilement. Une ampoule à consommation normale vous coutera environ 20€/an, alors eteignez la avec 'l'.'}
+    {name:'baignoireinfo', text:'Voici la baignoire. A chaque utilisation, elle utilise environ 180 litres d\'eau. Privilégiez donc la douche qui n\'utilise que 50 litres. C\'est plus rare que les autres appareils mais si le robinet de celle-ci rest ouvert, fermez le avec la touche 'b''}
+    {name:'frigoinfo', text:'Voici le réfrégirateur. '}
+    {name:'gazinfo', text:'Voici la gazinière.'}
+    {name:'lavaboinfo', text:'Voici le lavabo.'}
+    {name:'machinealaverinfo', text:'Voici la machine à laver.'}
+    {name:'microondeinfo', text:'Voici le micro-onde.'}
+    {name:'miroirinfo', text:'Voici le miroir.'}
+    {name:'ordinateurinfo', text:'Voici l\'ordinateur.'}
+    {name:'radiateurinfo', text:'Voici le radiateur.'}
+    {name:'radioinfo', text:'Voici la radio.'}
+    {name:'ventilateurinfo', text:'Voici le ventilateur.'}
 ];
