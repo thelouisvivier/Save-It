98b263b493d8a0f7dbe4d2ad497b12c05901d00c
LCgenial <35601827+Louis-ClementLangue@users.noreply.github.com>
2018-05-12 20:47:18 +0200

    Fixed Menu

--- saveit/js/states/menu/menu.js
+++ saveit/js/states/menu/menu.js
@@ -12,14 +12,13 @@ var Menu = {
     var fond = game.add.sprite(game.width/200,0, 'menu');
     fond.scale.setTo(game.width/3500);
     //Full screen mode
-    //essaye avec : .EXACT_FIT ou .RESIZE ou .USER_SCALE
     game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
     game.scale.setShowAll();
 
-    var buttonIntroduction = game.add.button((game.width/2.38), game.height/2.75, 'b_intro', this.startIntroduction, this, 1, 0, 2).scale.setTo(game.height/3850);
-    var buttonDemarrer = game.add.button((game.width/2.38) , game.height/2.18, 'b_demarrer', this.startStart, this, 1, 0, 2).scale.setTo(game.height/3850);
-    var buttonContinuer = game.add.button((game.width/2.38) , game.height/1.80, 'b_continuer', this.startContinuer, this, 1, 0, 2).scale.setTo(game.height/3850);
-    var buttonCredits = game.add.button((game.width/2.38) , game.height/1.52, 'b_credits', this.startCredit, this, 1, 0, 2).scale.setTo(game.height/3850);
+    var buttonIntroduction = game.add.button((game.width/2.45), game.height/2.50, 'b_intro', this.startIntroduction, this, 1, 0, 2).scale.setTo(game.height/3000);
+    var buttonDemarrer = game.add.button((game.width/2.45) , game.height/1.90, 'b_demarrer', this.startStart, this, 1, 0, 2).scale.setTo(game.height/3000);
+    var buttonContinuer = game.add.button((game.width/2.45) , game.height/1.53, 'b_continuer', this.startContinuer, this, 1, 0, 2).scale.setTo(game.height/3000);
+    var buttonCredits = game.add.button((game.width/2.45) , game.height/1.28, 'b_credits', this.startCredit, this, 1, 0, 2).scale.setTo(game.height/3000);
   },
 
   startIntroduction : function(){
