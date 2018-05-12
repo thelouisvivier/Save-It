6fcb4af6f887f6f35fb3efaa6d2f51fcbccfbec9
LCgenial <35601827+Louis-ClementLangue@users.noreply.github.com>
2018-05-12 22:56:02 +0200

    Move Pause

--- saveit/js/functions/pause.js
+++ saveit/js/functions/pause.js
@@ -3,8 +3,7 @@
 var menuPause = function(nameState){
   var groupePause = game.add.group();
   var touchePause = game.input.keyboard.addKey(Phaser.Keyboard.ESC);
-  var scaleBoutonX = 0.55;
-  var scaleBoutonY = 0.55;
+  var scaleBouton = game.width/2450;
   touchePause.onDown.add(()=>{
     console.log("Start Pause");
     var posMenuPauseX = game.camera.x + 270;
@@ -14,38 +13,38 @@ var menuPause = function(nameState){
       game.paused = false;
     }else{
       //La fenêtre qui va s'ouvrir
-      var spritePause = game.add.sprite(posMenuPauseX, posMenuPauseY, 'fondPause');
-      spritePause.scale.setTo(0.55,0.55);
+      var spritePause = game.add.sprite(game.camera.x + (game.width/4.50), game.camera.y + (game.height/4.20), 'fondPause');
+      spritePause.scale.setTo(game.width/2500);
       groupePause.add(spritePause);
       game.paused = true;
 
       //Bouton continuer
-      var continuerPause = game.add.button(posMenuPauseX + 20, posMenuPauseY + 360, 'b_reprendre', ()=>{
+      var continuerPause = game.add.button(game.camera.x + (game.width/4.20), game.camera.y + (game.height/1.40), 'b_reprendre', ()=>{
         console.log("Stop Pause");
         groupePause.removeAll(true, true);
         game.paused = false;
       }, this, 0, 0, 1);
-      continuerPause.scale.setTo(scaleBoutonX, scaleBoutonY);
+      continuerPause.scale.setTo(scaleBouton);
       groupePause.add(continuerPause);
 
       //Bouton recommencer
-      var recommencerPause = game.add.button(posMenuPauseX + 280, posMenuPauseY + 360, 'b_recommencer', ()=>{
+      var recommencerPause = game.add.button(game.camera.x + (game.width/2.35), game.camera.y + (game.height/1.40), 'b_recommencer', ()=>{
         console.log("Restart Level");
         groupePause.removeAll(true, true);
         game.paused = false;
         game.state.start(nameState);
       }, this, 0, 0, 1);
-      recommencerPause.scale.setTo(scaleBoutonX, scaleBoutonY);
+      recommencerPause.scale.setTo(scaleBouton);
       groupePause.add(recommencerPause);
 
       //Bouton sortie
-      var sortiePause = game.add.button(posMenuPauseX + 540, posMenuPauseY + 360, 'b_ecranTitre', ()=>{
+      var sortiePause = game.add.button(game.camera.x + (game.width/1.63), game.camera.y + (game.height/1.40), 'b_ecranTitre', ()=>{
         console.log("Start Menu");
         groupePause.removeAll(true, true);
         game.paused = false;
         game.state.start('MenuState');
       }, this, 0, 0, 1);
-      sortiePause.scale.setTo(scaleBoutonX, scaleBoutonY);
+      sortiePause.scale.setTo(scaleBouton);
       groupePause.add(sortiePause);
     }
   }, this);
