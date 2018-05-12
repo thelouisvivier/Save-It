a110fcc5ed8d0902f3243d0c731ac85efca247cc
Ployt <florentin.dehooghe@isen.yncrea.fr>
2018-05-12 16:17:29 +0200

    boutons + fond bien placés

--- saveit/js/states/menu/menu.js
+++ saveit/js/states/menu/menu.js
@@ -9,17 +9,17 @@ var Menu = {
 
   create : function(){
     this.stage.backgroundColor = '#000000';
-    var fond = game.add.sprite(0,0, 'menu');
+    var fond = game.add.sprite(game.width/200,0, 'menu');
     fond.scale.setTo(game.width/3500);
     //Full screen mode
     //essaye avec : .EXACT_FIT ou .RESIZE ou .USER_SCALE
     game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
     game.scale.setShowAll();
 
-    var buttonIntroduction = game.add.button((game.width/2) - 150, 400, 'b_intro', this.startIntroduction, this, 1, 0, 2).scale.setTo(0.25,0.25);
-    var buttonDemarrer = game.add.button((game.width/2) - 150, 500, 'b_demarrer', this.startStart, this, 1, 0, 2).scale.setTo(0.25,0.25);
-    var buttonContinuer = game.add.button((game.width/2) - 150, 600, 'b_continuer', this.startContinuer, this, 1, 0, 2).scale.setTo(0.25,0.25);
-    var buttonCredits = game.add.button((game.width/2) - 150, 700, 'b_credits', this.startCredit, this, 1, 0, 2).scale.setTo(0.25,0.25);
+    var buttonIntroduction = game.add.button((game.width/2.38), game.height/2.75, 'b_intro', this.startIntroduction, this, 1, 0, 2).scale.setTo(0.25,0.25);
+    var buttonDemarrer = game.add.button((game.width/2.38) , game.height/2.18, 'b_demarrer', this.startStart, this, 1, 0, 2).scale.setTo(0.25,0.25);
+    var buttonContinuer = game.add.button((game.width/2.38) , game.height/1.80, 'b_continuer', this.startContinuer, this, 1, 0, 2).scale.setTo(0.25,0.25);
+    var buttonCredits = game.add.button((game.width/2.38) , game.height/1.52, 'b_credits', this.startCredit, this, 1, 0, 2).scale.setTo(0.25,0.25);
   },
 
   startIntroduction : function(){
