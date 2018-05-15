f1dc2691e4d7b0807e3a1cb43f4f71ea778356ee
Louis VIVIER <22093201+louisvivier@users.noreply.github.com>
2018-05-15 22:34:53 +0200

    Fixed boutons "continuer"

--- saveit/js/states/menu/continuer.js
+++ saveit/js/states/menu/continuer.js
@@ -7,13 +7,25 @@ var Continuer = {
     //Full screen mode
     game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
     game.scale.setShowAll();
-    
-    var boutonRetour = game.add.button(game.width/1.20, game.height/1.20, 'retour', this.retourMenu, this, 1, 0);
-    var boutonNiveau1 = game.add.button(game.width/2.90, game.height/2.50, 'niveau1', this.accesNiveau1, this, 1, 0).scale.setTo(game.height/750);
-    var boutonNiveau2 = game.add.button(game.width/1.80, game.height/2.50, 'niveau2', this.accesNiveau2, this, 1, 0).scale.setTo(game.height/750);
-    var boutonNiveau3 = game.add.button(game.width/2.90, game.height/1.90, 'niveau3', this.accesNiveau3, this, 1, 0).scale.setTo(game.height/750);
-    var boutonNiveau4 = game.add.button(game.width/1.80, game.height/1.90, 'niveau4', this.accesNiveau4, this, 1, 0).scale.setTo(game.height/750);
-    var boutonNiveau5 = game.add.button(game.width/2.20, game.height/1.53, 'niveau5', this.accesNiveau5, this, 1, 0).scale.setTo(game.height/750);
+
+    var boutonRetour = game.add.button(game.width -50, game.height/1.20, 'retour', this.retourMenu, this, 1, 0);
+    var boutonNiveau1 = game.add.button((game.width/2) -50, game.height/2.50, 'niveau1', this.accesNiveau1, this, 1, 0);
+    var boutonNiveau2 = game.add.button((game.width/2)+50, game.height/2.50, 'niveau2', this.accesNiveau2, this, 1, 0);
+    var boutonNiveau3 = game.add.button((game.width/2)-50, game.height/1.90, 'niveau3', this.accesNiveau3, this, 1, 0);
+    var boutonNiveau4 = game.add.button((game.width/2)+50, game.height/1.90, 'niveau4', this.accesNiveau4, this, 1, 0);
+    var boutonNiveau5 = game.add.button(game.width/2, game.height/1.53, 'niveau5', this.accesNiveau5, this, 1, 0);
+    boutonNiveau1.anchor.x = 1;
+    boutonNiveau2.anchor.x = 0;
+    boutonNiveau3.anchor.x = 1;
+    boutonNiveau4.anchor.x = 0;
+    boutonNiveau5.anchor.x = 0.5;
+    boutonRetour.anchor.x = 1;
+    boutonNiveau1.scale.setTo(game.height/750);
+    boutonNiveau2.scale.setTo(game.height/750);
+    boutonNiveau3.scale.setTo(game.height/750);
+    boutonNiveau4.scale.setTo(game.height/750);
+    boutonNiveau5.scale.setTo(game.height/750);
+    boutonRetour.scale.setTo(game.height/750);
   },
 
   retourMenu : function(){
